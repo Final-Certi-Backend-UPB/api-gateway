17847dd0455194fec8b68196e65f420a62a677f8
enrique <enriqueadhemar@gmail.com>
2023-12-17 22:50:53 -0400

    swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { HttpModule } from '@nestjs/axios';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import rateLimit from 'express-rate-limit';
 import { auth } from './config/config';
+import { rateLimitConfig } from './config/rateLimiter';
 import { AuthController } from './controllers/auth.controller';
+import { MorganMiddleware } from './middlewares/morgan.middleware';
 import { DoctorProxyMiddleware } from './middlewares/proxy/doctor-proxy.middleware';
 import { PatientProxyMiddleware } from './middlewares/proxy/patient-proxy.middleware';
 import { UserProxyMiddleware } from './middlewares/proxy/user-proxy.middleware';
 import { AuthService } from './services/auth.service';
 import { EUREKA, EurekaService } from './services/eureka.service';
-import { MorganMiddleware } from './middlewares/morgan.middleware';
-import rateLimit from 'express-rate-limit';
-import { rateLimitConfig } from './config/rateLimiter';
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { env } from './config/config';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api/v1');
   app.enableCors();
+
+  const document = JSON.parse(
+    (await readFile(join(process.cwd(), '/docs/swagger.json'), { encoding: 'utf-8' })
+    ));
+  SwaggerModule.setup('/api-docs', app, document);
+
   await app.listen(env.port);
 }
 bootstrap();
