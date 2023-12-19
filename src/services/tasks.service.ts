ee58ea99400a5539d6eb58da779fc2ad3c40a55b
AnderMichael <andercaymi@gmail.com>
2023-12-19 12:05:31 -0400

    Lambda implemented

--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -1,12 +1,18 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
-  handleCron() {
-    this.logger.debug('Called every 10 seconds');
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async handleCron() {
+    try {
+      await axios.get("http://localhost:4566/restapis/dhoza8d127/test/_user_request_/miRecurso")
+    } catch (error) {
+      this.logger.log("The service is sleeping...")
+    }
   }
 }
+//EVERY_DAY_AT_MIDNIGHT
